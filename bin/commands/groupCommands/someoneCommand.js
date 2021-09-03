4870ece3177a4e7592158b9df90f505100934e35
Roi Klevansky <78471889+RoiKlevansky@users.noreply.github.com>
2021-09-03 23:32:16 +0300

    Added someone command

---
+++ bin/commands/groupCommands/someoneCommand.js
@@ -0,0 +1,22 @@
+const { randomFromArr } = require('../../utils/random');
+
+/**
+ * Tags a random user.
+ * @param {Message} message
+ * @return {Promise<void>}
+ */
+const procCommand = async (message) => {
+  const chat = await message.getChat();
+
+  if (!chat.isGroup) {
+    return;
+  }
+
+  let output = '';
+  const participant = randomFromArr(chat.participants);
+  const output = `@${participant.id.user} `;
+  const mentions = [participant];
+  await message.reply(output, undefined, { mentions: mentions });
+};
+
+module.exports = procCommand;
